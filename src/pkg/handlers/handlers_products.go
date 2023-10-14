88fac0f5b15695a1fbe81f3d643cc44366eed360
Matheus Barzon <matheusbarzon@gmail.com>
2023-10-14 13:10:47 -0300

    refactor: criando model e movendo logica para model produto

--- src/pkg/handlers/handlers_products.go
+++ src/pkg/handlers/handlers_products.go
@@ -3,11 +3,9 @@ package handlers_products
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"product/src/pkg/database"
+	models_products "product/src/pkg/models"
 	structs_products "product/src/pkg/structs"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -22,30 +20,7 @@ func postProducts(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	stament, err := database.Connection().Prepare(
-		`insert into products
-			(nome, gtin, inclusao)
-		values
-			(?, ?, ?)`)
-
-	if err != nil {
-		log.Printf(err.Error())
-		return
-	}
-
-	defer stament.Close()
-
-	result, err := stament.Execute(product.Nome, product.Gtin, product.Inclusao)
-
-	if err != nil {
-		log.Printf(err.Error())
-		return
-	}
-	product.Id = int64(result.InsertId)
-
-	defer result.Close()
-
-	println(result.InsertId)
+	models_products.Insert(&product)
 
 	jsonReturn, _ := json.Marshal(product)
 
@@ -62,40 +37,7 @@ func getByIdProducts(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	stament, err := database.Connection().Prepare(
-		`select id, nome, gtin, inclusao
-		   from products
-		  where id = ?`)
-
-	if err != nil {
-		log.Printf(err.Error())
-		return
-	}
-
-	defer stament.Close()
-
-	result, err := stament.Execute(reqId)
-
-	if err != nil {
-		log.Printf(err.Error())
-		return
-	}
-
-	var structReturn structs_products.Product
-
-	id, _ := strconv.ParseInt(reqId, 10, 64)
-	nome, _ := result.GetStringByName(0, "nome")
-	gtin, _ := result.GetStringByName(0, "gtin")
-	inclusao, _ := result.GetStringByName(0, "inclusao")
-
-	structReturn = structs_products.Product{
-		Id:       id,
-		Nome:     nome,
-		Gtin:     gtin,
-		Inclusao: inclusao,
-	}
-
-	defer result.Close()
+	var structReturn structs_products.Product = models_products.SelectById(reqId)
 
 	jsonReturn, err := json.Marshal(structReturn)
 	if err != nil {
@@ -111,35 +53,7 @@ func getByIdProducts(response http.ResponseWriter, request *http.Request) {
 func getProducts(response http.ResponseWriter, _ *http.Request) {
 	response.WriteHeader(http.StatusOK)
 
-	database.Connection()
-
-	result, err := database.Connection().Execute(
-		`select id, nome, gtin, inclusao
-		   from products`)
-
-	if err != nil {
-		log.Printf(err.Error())
-		return
-	}
-
-	var structReturn structs_products.Products
-
-	for i := range result.Values {
-
-		id, _ := result.GetIntByName(i, "id")
-		nome, _ := result.GetStringByName(i, "nome")
-		gtin, _ := result.GetStringByName(i, "gtin")
-		inclusao, _ := result.GetStringByName(i, "inclusao")
-
-		structReturn = append(structReturn, structs_products.Product{
-			Id:       id,
-			Nome:     nome,
-			Gtin:     gtin,
-			Inclusao: inclusao,
-		})
-	}
-
-	defer result.Close()
+	var structReturn = models_products.Select()
 
 	jsonReturn, err := json.Marshal(structReturn)
 	if err != nil {
